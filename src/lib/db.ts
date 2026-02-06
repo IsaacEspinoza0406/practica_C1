9ae0551edfac448144cc47156cd619d47f8f3169
Isaac <243751@ids.upchiapas.edu.mx>
2026-02-06 08:00:51 -0600

    Se creo una variable de entorno, para no exponer nuestras credenciales en el archivo .env

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,16 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Pool } from 'pg';
 
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error('Revise bien sus credenciales en DATABASE_URL en el archivo .env.local');
+}
+
 const pool = new Pool({
-  connectionString: 'postgres://isaac:123@127.0.0.1:5435/school_db',
+  connectionString,
 });
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const query = async (text: string, params?: any[]) => {
   return pool.query(text, params);
 };
